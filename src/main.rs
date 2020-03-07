5e541c1e617f8b30a6d8025dbc2338e262ab00c0
asnimansari <asnim96@gmail.com>
2020-03-07 17:39:18 +0530

    Displayed Hello world in emulator

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,22 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World";
+
 
 #[no_mangle] // tells that do not mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function will act as the entrypoint as linked looks for
     // function that `_start` when by default
+
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
