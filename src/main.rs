24b7504c163c63188c691c3e744ab9d7937094bc
asnimansari <asnim96@gmail.com>
2020-03-08 01:04:16 +0530

    Added a general writer for prinnting

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,10 @@ pub extern "C" fn _start() -> ! {
 
     // let vga_buffer = 0xb8000 as *mut u8;
 
-    vga_buffer::print_something();
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello Again").unwrap();
+    write!(vga_buffer::WRITER.lock()," , some number: {} {}", 42,1.337).unwrap();
+
 
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,6 +1,7 @@
 use volatile::Volatile;
 use core::fmt::{Write, Result};
-
+use lazy_static::lazy_static;
+use spin::Mutex;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -79,14 +80,34 @@ impl Writer {
         for byte in s.bytes() {
             match byte {
                 // printable ASCII byte or newline
-                0x20...0x7e | b'\n' => self.write_byte(byte),
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
                 // not part of printable ASCII range
                 _ => self.write_byte(0xfe),
             }
         }
     }
 
-    fn new_line(&mut self) { /* TODO */ }
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 1..BUFFER_WIDTH {
+                let charecter = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(charecter);
+            }
+        }
+
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 impl Write for Writer {
@@ -96,15 +117,10 @@ impl Write for Writer {
     }
 }
 
-
-pub fn print_something() {
-    let mut writer = Writer {
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new( Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello! ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
+    });
 }
